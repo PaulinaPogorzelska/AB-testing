fea41d94e2f81621166212dca261bb2bcb3bd15f
Paulina Pogorzelska <Paulinka@AppUnites-Mac-241.local>
2022-11-15 19:23:56 +0100

    check

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,10 @@ import useGoogleOptimize from '@react-hook/google-optimize'
 
 export default function Home() {
   useEffect(()=> {
+    if (window.dataLayer) {
+      console.log(33)
+      window.dataLayer.push({event: 'optimize.activate'});
+    }
     if (typeof window !== "undefined") {
     console.log(window?.google_optimize?.get('kMbu3v8SR5yh6Nao4eUjyg'))
     }
